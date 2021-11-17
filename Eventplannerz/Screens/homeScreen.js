626a0ac151e0d09ce4842ebb4276379b627c0b66
LucasSimper <lusi19ac@student.cbs.dk>
2021-11-17 09:29:27 +0100

    work bitch

---
+++ Eventplannerz/Screens/homeScreen.js
@@ -0,0 +1,9 @@
+function homeScreen() {
+    return (
+      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        <Text>Home!</Text>
+      </View>
+    );
+  }
+
+  module.export = homeScreen;
---
+++ Eventplannerz/Screens/settingsScreen.js
@@ -0,0 +1,9 @@
+function settingsScreen() {
+    return (
+      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        <Text>Settings!</Text>
+      </View>
+    );
+  }
+
+  module.export = settingsScreen;
