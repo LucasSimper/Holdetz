9cf83a62bb7ba1e71bce535262be4a82000740b8
LucasSimper <lusi19ac@student.cbs.dk>
2021-12-01 12:20:45 +0100

    your event changes

--- Eventplannerz/Screens/yourEventsScreen.js
+++ Eventplannerz/Screens/yourEventsScreen.js
@@ -6,19 +6,30 @@ import {
   Text,
   View,
   StatusBar,
+  Image,
 } from "react-native";
 import * as React from "react";
-import { getDatabase, ref, query, child, get, orderByChild, equalTo, onValue } from "firebase/database";
+import {
+  getDatabase,
+  ref,
+  query,
+  child,
+  get,
+  orderByChild,
+  equalTo,
+  onValue,
+} from "firebase/database";
 import { getAuth, onAuthStateChanged } from "firebase/auth";
 import { useEffect, useState } from "react";
 
 const yourEventsScreen = ({ navigation }) => {
-
   React.useLayoutEffect(() => {
     navigation.setOptions({
       headerRight: () => (
-        <Button onPress={() => navigation.navigate("Create Event")}
-                title="Create Event" />
+        <Button
+          onPress={() => navigation.navigate("Create Event")}
+          title="Create Event"
+        />
       ),
     });
   }, [navigation]);
@@ -28,30 +39,41 @@ const yourEventsScreen = ({ navigation }) => {
   const [events, setEvents] = useState({});
   const db = getDatabase();
 
-useEffect(() => {
-  const yourEventQuery = query(ref(db, 'events'), orderByChild('UserID'), equalTo(user.uid))
-  onValue(yourEventQuery, (snapshot) => {
-    if (snapshot.exists) {
-      console.log(snapshot.val());
-      setEvents(snapshot.val());
-    } else {
-      console.log("No data Available")
-    }
-})
-}, []);
+  useEffect(() => {
+    const yourEventQuery = query(
+      ref(db, "events"),
+      orderByChild("UserID"),
+      equalTo(user.uid)
+    );
+    onValue(yourEventQuery, (snapshot) => {
+      if (snapshot.exists) {
+        //console.log(snapshot.val());
+        setEvents(snapshot.val());
+      } else {
+        console.log("No data Available");
+      }
+    });
+  }, []);
 
   const eventsArr = Object.keys(events).map((key) => {
     events[key].id = key;
     return events[key];
   });
 
-  const Item = ({ title }) => (
+  const Item = ({ title, picture, date, time }) => (
     <View style={styles.item}>
+      <Image
+        style={{ width: 320, height: 120, alignSelf: "center" }}
+        source={{ uri: picture }}
+      />
       <Text style={styles.title}>{title}</Text>
+      <Text style={styles.dateTime}>Date: {date}  Time: {time}</Text>
     </View>
   );
 
-  const renderItem = ({ item }) => <Item title={item.id} />;
+  const renderItem = ({ item }) => (
+    <Item title={item.Name} picture={item.ImageURL} date={item.Date} time={item.Time}/>
+  );
 
   return (
     <SafeAreaView style={styles.container}>
@@ -60,7 +82,6 @@ useEffect(() => {
         renderItem={renderItem}
         keyExtractor={(item) => item.id}
       />
-
     </SafeAreaView>
   );
 };
@@ -71,14 +92,17 @@ const styles = StyleSheet.create({
     marginTop: StatusBar.currentHeight || 0,
   },
   item: {
-    backgroundColor: "#f9c2ff",
-    padding: 20,
     marginVertical: 8,
     marginHorizontal: 16,
   },
   title: {
-    fontSize: 32,
+    fontSize: 20,
+    alignSelf: "center",
   },
+  dateTime: {
+    fontSize: 15,
+    alignSelf: "center"
+  }
 });
 
 export default yourEventsScreen;
